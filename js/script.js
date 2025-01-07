635ae763d214a6e9ccb2dea18bae187b92dfef7c
plntry <nedilko7b@gmail.com>
2025-01-07 18:09:10 +0200

    added transparent navbar on scroll

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ function toggleMobileMenu() {
   mobileMenu.classList.toggle("active");
 }
 
+// toggle mobile menu
 document.addEventListener("DOMContentLoaded", function () {
   const navbarToggleButton = document.querySelector(
     ".navbar .mobile-menu-toggle"
@@ -13,4 +14,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
   navbarToggleButton.addEventListener("click", () => toggleMobileMenu());
   mobileMenuCloseButton.addEventListener("click", () => toggleMobileMenu());
+});
+
+// change navbar background on scroll
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar");
+
+  if (window.scrollY > 0) {
+    navbar.classList.add("navbar-scroll");
+  } else {
+    navbar.classList.remove("navbar-scroll");
+  }
 });
