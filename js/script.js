f13d463335dfec6176ffd9faf1f305e1dce7b59f
plntry <nedilko7b@gmail.com>
2025-01-07 17:34:33 +0200

    added final styles for mobile navbar menu

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const toggleButton = document.querySelector(".navbar .mobile-menu-toggle");
+function toggleMobileMenu() {
   const mobileMenu = document.querySelector(".navbar .mobile-menu-items");
+  mobileMenu.classList.toggle("active");
+}
 
-  toggleButton.addEventListener("click", function () {
-    mobileMenu.classList.toggle("active");
-  });
+document.addEventListener("DOMContentLoaded", function () {
+  const navbarToggleButton = document.querySelector(
+    ".navbar .mobile-menu-toggle"
+  );
+  const mobileMenuCloseButton = document.querySelector(
+    ".navbar .mobile-menu-close-btn"
+  );
+
+  navbarToggleButton.addEventListener("click", () => toggleMobileMenu());
+  mobileMenuCloseButton.addEventListener("click", () => toggleMobileMenu());
 });
